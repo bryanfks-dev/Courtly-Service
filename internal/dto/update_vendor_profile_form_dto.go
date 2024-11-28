c55401968d93dc2d12629acae0b47f3d6f8c6479
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-11-28 17:53:30 +0700

    rename struct

--- internal/dto/update_vendor_profile_form_dto.go
+++ internal/dto/update_vendor_profile_form_dto.go
@@ -1,8 +1,8 @@
 package dto
 
-// UpdateCurrentVendorProfileDTO is a struct that defines the data
+// UpdateVendorProfileFormDTO is a struct that defines the data
 // needed to update the current vendor profile.
-type UpdateCurrentVendorProfileDTO struct {
+type UpdateVendorProfileFormDTO struct {
 	OpenTime  string `json:"open_time"`
 	CloseTime string `json:"close_time"`
 }
