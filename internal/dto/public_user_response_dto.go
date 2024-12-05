c0eeb9420579fea7e5d0d4726850a3403cf2c156
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-12-05 22:13:00 +0700

    remove unused dto

--- internal/dto/public_user_response_dto.go
+++
@@ -1,7 +0,0 @@
-package dto
-
-// PublicUserResponseDTO is a struct that represents the public user response dto.
-type PublicUserResponseDTO struct {
-	// Users is a field that represents the public user.
-	User *PublicUserDTO `json:"user"`
-}
