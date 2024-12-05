0d468ffef1551381e47db647098fb2c6b047fe8b
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-12-05 12:11:39 +0700

    remove unused dto

--- internal/dto/review_response_dto.go
+++
@@ -1,7 +0,0 @@
-package dto
-
-// ReviewResponseDTO is a struct that defines the data transfer object for a review response.
-type ReviewResponseDTO struct {
-	// Review is the review data transfer object
-	Review *ReviewDTO `json:"review"`
-}
